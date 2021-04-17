fd50f2d8ad5af4c614064d1eccc44f82ad351a05
Crowdbotics <team@crowdbotics.com>
2021-04-17 06:33:54 +0000

    Update models

--- backend/dating/admin.py
+++ backend/dating/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Setting, Like, UserPhoto, Match, Dislike, Inbox, Profile
+from .models import Dislike, Inbox, Like, Match, Profile, Setting, UserPhoto
 
 admin.site.register(Inbox)
 admin.site.register(Profile)
